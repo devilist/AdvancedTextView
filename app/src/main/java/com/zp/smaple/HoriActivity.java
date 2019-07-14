7d3b30a273cd38e570047fcda50b66be63467d45
devilist <bit_zengpu@163.com>
2019-07-14 16:49:45 +0800

    merge to androidx

--- app/src/main/java/com/zp/smaple/HoriActivity.java
+++ app/src/main/java/com/zp/smaple/HoriActivity.java
@@ -1,15 +1,32 @@
+/*
+ * Copyright  2019  zengp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.zp.smaple;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.devilist.advancedtextview.ActionMenu;
 import com.devilist.advancedtextview.CustomActionMenuCallBack;
